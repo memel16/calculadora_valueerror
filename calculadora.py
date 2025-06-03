79e0b756bbed2b8619f943a13fac8d2e6273b1d9
Mellyssa Ferreira <mellyssaf881@gmail.com>
2025-06-03 17:45:49 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,31 +1,11 @@
 try:
 
-    num1=int(input("Digite o numero 1 "))
-    num2=int(input("Digite o numero 2"))
 
+   Número = int (input ("Primeiro número :"))
+   Número2 = int (input("Segundo número : "))
 
+   print (f" {Número} x {Número2} = {Número * Número2}")
 
-    print("Escolha uma operação:")
-    print("1. +")
-    print("2. -")
-    print("3. *")
-    print("4. /")
+except:
+   print("Erro no número.")
 
-operacao = input("Digite o símbolo da operação (+, -, *, /): ")
-
-if operacao == '+':
-    resultado = num1 + num2
-elif operacao == '-':
-    resultado = num1 - num2
-elif operacao == '*':
-    resultado = num1 * num2
-elif operacao == '/':
-    resultado == num1 / num2
-
-else:
-    resultado = "Erro:"
-
-    print("Resultado:", resultado)
-
-    except ValueError:
-print(f"ocorreu um erro durante a operação no {ValueError}")
